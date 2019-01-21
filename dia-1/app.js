a0dfe1a0befe68950f1e96ae145f417f2897e5c0
[Anxo] <[anxobreogan@hotmail.com]>
2019-01-21 21:32:35 +0100

    ejercicios1

---
+++ dia-1/app.js
@@ -0,0 +1,43 @@
+/*
+ * Aquí podéis hacer los ejercicios y
+ * practicar Javascript!
+ */
+/*let nombre = "anxo";
+let Nombre = "anxo";
+
+const name = "kjdkkd";
+
+const color_orange = "#FF7F00";
+
+let edad = 36;*/
+
+/*let admin;
+let name;
+
+name = "jhon";
+admin = name;
+
+alert(admin);*/
+
+/*let ourPlanetName = "tierra";
+let currentUserName="John";*/
+
+
+
+console.log("mi nombre es " + nombre + "edad" + edad);
+console.log(`mi nombre es ${nombre} nombre`);
+console.log(name);
+[1, 2].forEach(alert); //esto es otro comentario
+
+//comentarios
+/*hola caracol
+lajfjkañ
+dkfjañjfñajkf*/
+/** fkjañlkf
+ *
+ *
+ *
+ *
+ * tararí que te vi
+ */
+/* "use strict", para hacer metodos nuevos y no romper los antiguos*/
